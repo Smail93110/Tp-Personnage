fc7787a30f782e9e8476e2926f696acd09074697
Smail93110 <110185849+Smail93110@users.noreply.github.com>
2024-01-30 11:52:38 +0100

    Update jeuPersonnage.java

--- jeuPersonnage.java
+++ jeuPersonnage.java
@@ -1,22 +1,51 @@
 import Personnage.*;
-public class jeuPersonnage{
+
+public class jeuPersonnage {
     public static void main(String args[]) {
-       Personnage UnSorcier = new Personnage("Sorcier",70);
-       System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
+        Personnage UnSorcier = new Personnage("Sorcier", 70);
 
-       Personnage UnMagicien = new Personnage("Magicien",100);
-       System.out.println("La vie du magicien est " + UnMagicien.LaVie());
+        Personnage UnMagicien = new Personnage("Magicien", 100);
 
-       System.out.println("Le nom du sorcier = " + UnSorcier.LeNom());
-       System.out.println("Le nom du magicien = " + UnMagicien.LeNom());
+        while (UnSorcier.LaVie() >0 && UnMagicien.LaVie() > 0) {
+        System.out.println("le jeu continue");
 
-       System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
-       UnSorcier.AugmenterVie(10);
-       System.out.println("Apres la vie du sorcier est " + UnSorcier.LaVie());
+        System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
 
-       System.out.println("le magicien attaque le sorcier ...");
-       UnMagicien.Attaque(UnSorcier,40);
-       System.out.println("Atak La vie du magicien est " + UnMagicien.LaVie());
-       System.out.println("Atak La vie du sorcier est " + UnSorcier.LaVie());
-	}
+        System.out.println("La vie du magicien est " + UnMagicien.LaVie());
+
+        // System.out.println("Le nom du sorcier = " + UnSorcier.LeNom());
+        // System.out.println("Le nom du magicien = " + UnMagicien.LeNom());
+
+        // System.out.println("La vie du sorcier est " + UnSorcier.LaVie());
+        // UnSorcier.AugmenterVie(10);
+
+        // System.out.println("Apres la vie du sorcier est " + UnSorcier.LaVie());
+
+
+        double random = Math.random();
+        if(random == 0){
+            System.out.println("le magicien attaque le sorcier ...");
+            UnMagicien.Attaque(UnSorcier, 40);
+
+        } else{
+            System.out.println("le sorcier attaque le magicien ...");
+           UnSorcier.Attaque(UnMagicien, 40);
+
+        }
+
+
+        // if (UnMagicien == 0) {
+
+
+        //     // System.out.println(vie);
+
+        // }
+
+        // System.out.println("le magicien attaque le sorcier ...");
+        // UnMagicien.Attaque(UnSorcier, 40);
+        System.out.println("Atak La vie du magicien est " + UnMagicien.LaVie());
+        System.out.println("Atak La vie du sorcier est " + UnSorcier.LaVie());
+
+    }
+}
 }
